12e0d261de31fd6608e4340fe10a610262f0388b
yik <svmboy@gmail.com>
2014-12-20 16:51:22 +0800

    Tower game gen monster and collision done

---
+++ Assets/Scripts/SummonMonster.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var id: int;
+var prefab: GameObject;
+
+function summonsummon () {
+   Debug.Log("Monster Summoned");
+   Instantiate(prefab,GameObject.Find("Genmonster").transform.position, Quaternion.identity);
+
+}
---
+++ Assets/Scripts/Tower.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var hp: int=100;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
