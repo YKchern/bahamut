238282e8132064e8fbaaa1e3747c7be0b861fde3
ykchern <svmboy@gmail.com>
2014-12-27 15:51:47 +0800

    Tower EnemyAI Part 2

--- Assets/Scripts/EnemyAI.js
+++ Assets/Scripts/EnemyAI.js
@@ -2,14 +2,28 @@
 
 var GenEnemyPoint: GameObject;
 var prefab: GameObject[];
-
+var waveNo: int=5;
 function Start () {
-   for(var i=0;i<5;i++){
-       Instantiate(prefab[Random.Range(0,3)],GenEnemyPoint.transform.position, Quaternion.identity); 
-       yield WaitForSeconds(.3);
-   }
+
+  //  for(var i=0;i<5;i++){
+  //     Instantiate(prefab[Random.Range(0,3)],GenEnemyPoint.transform.position, Quaternion.identity); 
+  //     yield WaitForSeconds(.3);
+  // }
+  
+  for(var i=0;i<waveNo;i++){
+   genEnemy(5,0,3,.3);
+   yield WaitForSeconds(3);
+   genEnemy(2,0,3,.5);
+  }
+  
 }
 
-function Update () {
+function genEnemy(eAmt: int, startIdx:int , endIdx:int, wait: float){
+
+      for(var i=0;i<eAmt;i++){
+       Instantiate(prefab[Random.Range(startIdx,endIdx)],GenEnemyPoint.transform.position, Quaternion.identity); 
+       yield WaitForSeconds(wait);
+   }
+
 
 }
